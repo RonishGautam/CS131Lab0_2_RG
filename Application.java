9e2a9f39d9f358e34e7abf08d690c10f986d649b
RonishGautam <122306969+RonishGautam@users.noreply.github.com>
2023-02-08 16:31:30 -0500

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,15 @@
+public class Application {
+
+	public static void main(String[] args) {
+		ArrayFun fun = new ArrayFun();
+	
+		fun.forword();
+		System.out.println("\n");
+		fun.backword();
+		System.out.println("\n");
+		System.out.println("\n");
+		fun.sum();
+		
+	}
+
+}
---
+++ Square.java
@@ -0,0 +1,13 @@
+
+public class Square {
+	public double side;
+
+	public Square(double side) {
+		this.side=side;
+	
+	}
+	public double getArea() {
+		return side*side;
+	}
+
+}
